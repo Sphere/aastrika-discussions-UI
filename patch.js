0e58767c9dd291e40c2bbd313f80253f157362e1
vpPavithra <pavithra.prakash@tarento.com>
2025-08-21 17:10:50 +0530

    unused file

--- patch.js
+++
@@ -1,6 +0,0 @@
-const { SourceMapConsumer } = require("source-map");
-const path = require("path");
-
-SourceMapConsumer.initialize({
-  "lib/mappings.wasm": path.join(__dirname, "node_modules/source-map/lib/mappings.wasm"),
-});
