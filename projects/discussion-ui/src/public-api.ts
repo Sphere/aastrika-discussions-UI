5306c53f3b2e7f966fc1e8c503f24a8f83964335
vpPavithra <pavithra.prakash@tarento.com>
2025-08-25 12:18:32 +0530

    ng 13 migration fix

--- projects/discussion-ui/src/public-api.ts
+++ projects/discussion-ui/src/public-api.ts
@@ -1,17 +1,31 @@
-/*
- * Public API Surface of discussion-ui
- */
-
 export * from './lib/discussion-ui.module';
+export * from './lib/components/components.module';
+
+// Services
 export * from './lib/discussion-events.service';
 export * from './lib/models/discussion-config.model';
-export * from './lib/services/config.service'
+export * from './lib/services/config.service';
 export * from './lib/services/discussion.service';
-export * from './lib/wrapper-navigate.service'
-export * from './lib/discussion-events.service'
-export * from './lib/events.service'
-export * from './lib/navigation-service.service'
-export * from './lib/services/abstract-config.service'
-export * from './lib/wrapper/category-widget/category-widget.component'
-export * from './lib/wrapper/base-wrapper/base-wrapper.component'
-export * from './lib/wrapper/tags-widget/tags-widget.component'
+export * from './lib/wrapper-navigate.service';
+export * from './lib/events.service';
+export * from './lib/navigation-service.service';
+export * from './lib/services/abstract-config.service';
+
+// Wrapper Components
+export * from './lib/wrapper/category-widget/category-widget.component';
+export * from './lib/wrapper/base-wrapper/base-wrapper.component';
+export * from './lib/wrapper/tags-widget/tags-widget.component';
+
+// UI Components (all the ones flagged in the error)
+export * from './lib/components/side-pannel/side-pannel.component';
+export * from './lib/components/discuss-home/discuss-home.component';
+export * from './lib/components/discuss-category/discuss-category.component';
+export * from './lib/components/discuss-tags/discuss-tags.component';
+export * from './lib/components/my-discussion/my-discussion.component';
+export * from './lib/components/discussion-details/discussion-details.component';
+export * from './lib/components/discuss-start/discuss-start.component';
+export * from './lib/components/discuss-all/discuss-all.component';
+export * from './lib/components/tag-all-discussion/tag-all-discussion.component';
+export * from './lib/components/trending-tags/trending-tags.component';
+export * from './lib/components/leader-board/leaderboard-component';
+export * from './lib/elements/reply-comment/reply-comment.component';
